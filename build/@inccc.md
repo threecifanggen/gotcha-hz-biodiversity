ff3b2da84c3a1b0da9fdf234ab4d6c8b182d5e94
3gee <huangbaochenwo@live.com>
2024-02-05 20:13:21 +0800

    add pages

---
+++ build/@inccc.md
@@ -0,0 +1,22 @@
+---
+title: "@inccc"
+aliases:
+  - 天师
+---
+
+## 基本信息
+
+AKA:inccc|天师
+
+邮箱: 2291804015@qq.com
+
+## 社交媒体
+
+INaturalist: [inccc的主页](https://www.inaturalist.org/people/incccc)
+
+## 技能树
+
+@天师 擅长如下能力：
+
+* 大树蛙 及 福建竹叶青 的拍摄
+* 微距柔光技能的研发
--- package.py
+++ package.py
@@ -1,23 +1,28 @@
 from pathlib import Path
 import typing as t
 
-def search_dir():
-    return Path('content').glob('**/*.md')
 
-def package(paths:t.Generator[Path, None, None]):
+def search_dir():
+    return Path("content").glob("**/*.md")
+
+
+def package(paths: t.Generator[Path, None, None]):
     content_total = ""
     for p in paths:
         print(p)
-        content_total += "\n" + (
+        content = (
             p.read_text(encoding="utf-8")
-            .replace("[[", "").replace("]]", "")
+            .replace("[[", "")
+            .replace("]]", "")
             .replace(" #", "")
             .replace(" @", "")
         )
+        content_total += "\n" + content
+        (Path() / "build" / p.name).write_text(content, encoding="utf-8")
     return content_total
+
 
 if __name__ == "__main__":
     paths = search_dir()
     content = package(paths)
-    (Path() / "build" / 'content.md').write_text(content, encoding="utf-8")
-
+    (Path() / "build" / "content.md").write_text(content, encoding="utf-8")
